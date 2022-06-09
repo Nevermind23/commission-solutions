4c03cda25de5aad58abc476826643abb6611ebd3
Nevermind23 <tyeshela7@gmail.com>
2022-06-09 14:15:31 +0400

    Updated default precision

--- src/helper.php
+++ src/helper.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 if (!function_exists('round_up')) {
-    function round_up($number, $precision = 2)
+    function round_up($number, $precision = 0)
     {
         $divider = 10 ** $precision;
 
